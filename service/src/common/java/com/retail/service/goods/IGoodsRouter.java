50ad00c5a089c7ab3a78cbf3590be61e11bb6cba
guangxian <guangxian@youzan.com>
2019-03-11 09:31:24 +0800

    增加接口

--- service/src/common/java/com/retail/service/goods/IGoodsRouter.java
+++ service/src/common/java/com/retail/service/goods/IGoodsRouter.java
@@ -4,5 +4,5 @@ public interface IGoodsRouter {
     /**
      * 选择多单位选择fragment
      */
-    String GET_UNIT_SELECT_FRAGMENT = "get_unit_select_fragment";
+    String Goods_GET_UNIT_SELECT_FRAGMENT = "goods_get_unit_select_fragment";
 }
--- service/src/common/java/com/retail/service/goods/model/IGoodsData.java
+++ service/src/common/java/com/retail/service/goods/model/IGoodsData.java
@@ -1,10 +1,21 @@
 package com.retail.service.goods.model;
 
+/**
+ * 商品实体对象
+ */
 public interface IGoodsData {
-
+    /**
+     * 商品id
+     */
     long getId();
 
+    /**
+     * 商品名称
+     */
     String getName();
 
+    /**
+     * 商品价格
+     */
     Long getPrice();
 }

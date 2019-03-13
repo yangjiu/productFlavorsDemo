fd04cbcc56c9f2572457ac26e7dfd176ee8534d7
guangxian <guangxian@youzan.com>
2019-03-13 16:21:20 +0800

    备份

---
+++ member/src/common/java/com/retail/member/model/ModelPlugin.java
@@ -0,0 +1,28 @@
+package com.retail.member.model;
+
+import com.retail.member.service.MemberPadServiceImpl;
+import com.retail.member.service.MemberWrapServiceImpl;
+import com.retail.service.member.service.IMemberPadService;
+import com.retail.service.member.service.IMemberWrapperService;
+import com.retail.service.util.ServiceManagerFactory;
+//
+//public class ModelCommonPlugin {
+//    @AppStart
+//    public void onAppStarted() {
+//        ServiceManagerFactory.getInstance().put(IMemberWrapperService.class,MemberWrapServiceImpl.class);
+//    }
+//}
+//
+//public class ModelPadPlugin {
+//    @AppStart
+//    public void onAppStarted() {
+//        ServiceManagerFactory.getInstance().put(IMemberPadService.class,MemberPadServiceImpl.class);
+//    }
+//}
+//
+//public class ModelPhonePlugin {
+//    @AppStart
+//    public void onAppStarted() {
+//        ServiceManagerFactory.getInstance().put(IMemberPhoneService.class,MemberPhoneServiceImpl.class);
+//    }
+//}

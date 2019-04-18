11e9384c9e055e13a8dad53a41b7416b307d0038
guangxian <guangxian@youzan.com>
2019-04-18 11:30:57 +0800

    备份

--- module_mediator/src/main/java/main/IService.java
+++ module_mediator/src/main/java/main/IService.java
@@ -2,12 +2,13 @@ package main;
 
 /**
  * Service 接口
- *
  */
 @SuppressWarnings({"WeakerAccess", "unused"})
 public interface IService {
 
-    /** 用来标示同一个Service的不同实现 */
+    /**
+     * 用来标示同一个Service的不同实现
+     */
     String getName();
 
 }
